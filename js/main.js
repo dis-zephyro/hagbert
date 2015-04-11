06e393acd24c36f315b505de0482b82d3c29235a
dis-zephyro <zephyro@yandex.ru>
2015-04-11 12:39:18 +0300

    start

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(".order").fancybox({
 });
 
 
-
+/*
 jQuery(document).ready(function(){
     jQuery(".advantage").accordion({
         collapsible: true,
@@ -14,6 +14,24 @@ jQuery(document).ready(function(){
             $(ui.oldHeader).show();
             console.log("Была активной панель: " + $(ui.oldHeader).text());
             console.log("Стала активной панель: " + $(ui.newHeader).text());
+        }
+    });
+});
+*/
+
+$(".scroll").each(function () { // анимация по скролу(используйте этот) достаточно добавить анимируемому блоку класс 'scroll' а css анимацию писать так: '.animated.класс_блока'
+    var block = $(this);
+    $(window).scroll(function() {
+        var top = block.offset().top;
+        var bottom = block.height()+top;
+        top = top - $(window).height();
+        var scroll_top = $(this).scrollTop();
+        if ((scroll_top > top) && (scroll_top < bottom)) {
+            if (!block.hasClass("animated")) {
+                block.addClass("animated");
+            }
+        } else {
+            block.removeClass("animated");
         }
     });
 });
