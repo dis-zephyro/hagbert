4d40f1eb382c6322b799f03f0e6675b95357a8fb
dis-zephyro <zephyro@yandex.ru>
2015-04-28 18:00:38 +0300

    start

--- js/main.js
+++ js/main.js
@@ -37,3 +37,62 @@ $(".scroll").each(function () {
     });
 });
 
+
+$(".scroll2").each(function () {
+    var block = $(this);
+    $(window).scroll(function() {
+        var top = block.offset().top;
+        var bottom = block.height()+top;
+        top = top - $(window).height();
+        var scroll_top = $(this).scrollTop();
+        if ((scroll_top > top) && (scroll_top < bottom)) {
+            if (!block.hasClass("animated")) {
+                block.addClass("animated");
+            }
+        } else {
+            block.removeClass("animated");
+        }
+    });
+});
+
+
+
+/* Таймер */
+
+
+$(function(){
+
+    var note = $('#note'),
+        ts = new Date(2015, 4, 1),
+        newYear = true;
+
+    if((new Date()) > ts){
+        // Задаем точку отсчета для примера. Пусть будет очередной Новый год или дата через 10 дней.
+        // Обратите внимание на *1000 в конце - время должно задаваться в миллисекундах
+        ts = (new Date()).getTime() + 10*24*60*60*1000;
+        newYear = false;
+    }
+
+    $('#countdown').countdown({
+        timestamp	: ts,
+        callback	: function(days, hours, minutes, seconds){
+
+            var message = "";
+
+            message += "Дней: " + days +", ";
+            message += "часов: " + hours + ", ";
+            message += "минут: " + minutes + " и ";
+            message += "секунд: " + seconds + " <br />";
+
+            if(newYear){
+                message += "осталось до Нового года!";
+            }
+            else {
+                message += "осталось до момента через 10 дней!";
+            }
+
+            note.html(message);
+        }
+    });
+
+});
